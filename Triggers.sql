ef2bf2da833571cdafb8f7b17c2ab9fdee00c0cd
Sofia-Arias017 <189870116+Sofia-Arias017@users.noreply.github.com>
2025-07-02 22:27:10 -0500

    feat: :sparkles: Ejercicio 2

--- Triggers.sql
+++ Triggers.sql
@@ -13,3 +13,29 @@ END $$
 DELIMITER ;
 
 INSERT INTO detalle_pedido (pedido_id, cantidad)VALUES (1, 0);
+
+-- 2. Descontar stock tras agregar ingredientes extra (Trigger `AFTER INSERT`).
+
+DELIMITER $$
+CREATE TRIGGER trg_after_insert_ingredientes_extra
+AFTER INSERT ON ingredientes_extra
+FOR EACH ROW
+BEGIN
+    DECLARE stock_actual INT;
+
+    SELECT stock INTO stock_actual
+    FROM ingrediente
+    WHERE id = NEW.ingrediente_id;
+
+    IF stock_actual < NEW.cantidad THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Stock insuficiente para este ingrediente.';
+    END IF;
+
+    UPDATE ingrediente
+    SET stock = stock - NEW.cantidad
+    WHERE id = NEW.ingrediente_id;
+END $$
+DELIMITER ;
+
+INSERT INTO ingredientes_extra (detalle_id, ingrediente_id, cantidad)VALUES (1, 2, 1);
